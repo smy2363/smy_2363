2ebc13e4dd400a282048562d66853c28021fc0b8
nevnerver <nevnerver@naver.com>
2024-06-25 12:46:19 +0900

    실기평가

--- 0624study/static/javascript/init.js
+++ 0624study/static/javascript/init.js
@@ -1,21 +1,23 @@
 // init.js
 
-// 초기 세팅
-
+// 초기 세팅 
 let boardInit; // 게임판 초기화 함수
 let stateInit; // 게임 현황 초기화 함수
-let imgLocation; // 이미지 배치
+let imgLocation; // 이미지 배치 함수
 let start; // 게임 시작 함수
 
-// 전역변수
-
-let score = 0; // 점수
-let time = 0; // 시간
+//전역변수
+let score = 0; //점수
+let time = 0; //시간
 let clickCount = 0; // 클릭횟수
-const imgName = ["두리안.jpg", "람부탄.jpg", "망고.jpg", "바나나.jpg", "배.png", "수박.jpg", "용과.jpg", "자두.jpg", "파파야.jpg"]
-let imgPlace = []; // 이미지 배치값(1,2,3,4,5)
+const imgName = ["두리안.jpg", "람부탄.jpg", "망고.jpg", "바나나.jpg", "배.png",
+    "수박.jpg", "용과.jpg", "자두.jpg", "파파야.jpg"
+]
+let imgPlace = []; // 이미지 배치 값 (1,2,3,4,5,...);
 let selectImg = []; // 선택 이미지 인덱스
 let imgCount = 6; // 사용할 이미지 갯수
+
+
 
 
 $(function () {
@@ -31,44 +33,39 @@ boardInit = function () {
     });
 }
 
-// 게임 시작 함수
+//게임 시작 함수
 start = function () {
     stateInit();
     imgLocation();
     $(".item").on("click", imgClick);
-    timeStart(); //게임 진행 시간 시작!!
-
+    timeStart(); // 게임 진행 시간 시작!!
 }
 
-// 게임현황 초기화 함수
+//게임현황 초기화함수
 stateInit = function () {
-    // $("#state").show();
-
+    //$("#state").show();
     $("#gameStart").addClass("hide");
     $("#state").removeClass("hide");
 
-
-    $("#score").text(`점수 : ${score}`);
+    $("#score").text(`점수 : ${score}점`);
     $("#step").text(`${time}초`);
     $("#click").text(`클릭횟수 : ${clickCount}/30`);
 }
 
-// 이미지 배치 함수
-
+//이미지 배치 함수
 imgLocation = function () {
 
     var temp = [];
-    while (temp.length != imgCount) { //12칸이므로 사용할 이미지 6장 구하기(랜덤하게)
+    while (temp.length != imgCount) {  // 12칸이므로 사용할 이미지 6장 구하기(랜덤하게)
         var tempNum = Math.floor(Math.random() * imgName.length);
         if (temp.indexOf(tempNum) == -1)
             temp.push(tempNum);
     }
-
     console.log(temp);
 
-    imgPlace = temp.concat(temp); //6개의 랜덤값을 두개 합쳐서 저장
+    imgPlace = temp.concat(temp); // 6개의 랜덤값을 두개 합쳐서 저장
     console.log(imgPlace);
-    imgPlace = shuffle(); //imgPlace에 저장된 값을 섞어주기 위한 함수실행
+    imgPlace = shuffle(); // imgPlace에 저장된 값을 섞어주기 위한 함수실행
     console.log(imgPlace);
 
     $(".item").each(function (i) {
@@ -81,7 +78,6 @@ imgLocation = function () {
     }, 1000);
 
 }
-
 function shuffle() {
     for (var i = imgPlace.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
